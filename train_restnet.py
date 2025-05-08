31346020095ac2a280ff22b6aacefe90031e98b3
Le Phu <phulesieunangluc@gmail.com>
2025-05-08 19:08:47 +0700

    aaa

--- train_restnet.py
+++ train_restnet.py
@@ -7,16 +7,17 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import multiprocessing
 
+
 # Thêm hỗ trợ multiprocessing cho Windows
 def train_resnet():
     # ====== Config ======
-    data_dir = 'data/processed'
+    data_dir = 'data/processe'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
+
     # Kiểm tra thư mục dữ liệu
     if not os.path.exists(data_dir):
         raise FileNotFoundError(f"Thư mục dữ liệu '{data_dir}' không tồn tại!")
-        
+
     num_classes = len(os.listdir(data_dir))
 
     # ====== Dataloader ======
@@ -63,12 +64,13 @@ def train_resnet():
             total += labels.size(0)
             correct += predicted.eq(labels).sum().item()
 
-        print(f"[Epoch {epoch+1}] Loss: {running_loss:.3f} | Accuracy: {correct/total:.2%}")
+        print(f"[Epoch {epoch + 1}] Loss: {running_loss:.3f} | Accuracy: {correct / total:.2%}")
 
     # ====== Save model ======
     os.makedirs("models", exist_ok=True)
     torch.save(model.state_dict(), "models/resnet50_face_classifier.pth")
     print("✅ Đã lưu mô hình ResNet-50.")
+
 
 # Thêm cấu trúc bảo vệ cho multiprocessing
 if __name__ == '__main__':
